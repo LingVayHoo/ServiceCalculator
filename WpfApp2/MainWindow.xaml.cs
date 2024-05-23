e5650d5c4c515613a2c1da2b7a86b4b9ceb1637b
Андрей <i@vakarianx.ru>
2024-05-23 19:06:18 +0300

    cost-splited-to-delivery-ascent

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -1,15 +1,6 @@
 ﻿using ServiceCalculator;
-using System.Diagnostics;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp2
 {
@@ -62,13 +53,19 @@ namespace WpfApp2
 
         private void ResultButton_Click(object sender, RoutedEventArgs e)
         {
+            ClearResults();
             var km = KmCountText.Text != string.Empty ? KmCountText.Text : 0.ToString();
             if (Int32.TryParse(km, out var result))
             {
                 var res = deliveryCalculator.Calculate(result);
                 res *= CheckWeight();
                 res = priceChecker.Check(settings.IsSmallType, result) >= 0 ? priceChecker.Check(settings.IsSmallType, result) : res;
-                if ((bool)NeedAscent.IsChecked) res += CalculateAscent();
+                if ((bool)NeedAscent.IsChecked)
+                {
+                    ResultTextAscent.Text = CalculateAscent().ToString();
+                    if (CalculateAscent() >= 1000000) CommentsText.Text = "Не удалось посчитать подъем из-за размеров коробок! Нужен подсчет вручную! ";
+                }
+                
                 ResultText.Text = res.ToString();
             }            
         }
@@ -88,9 +85,18 @@ namespace WpfApp2
             return 1;
         }
 
+        private void ClearResults()
+        {
+            ResultText.Text = string.Empty;
+            ResultTextAscent.Text = string.Empty;
+            CommentsText.Text = string.Empty;
+        }
+
         private float CalculateAscent()
         {
             float result = 0;
+            if(WeightText.Text == string.Empty) CommentsText.Text = "Введите вес!";
+            if (FloorNumberText.Text == string.Empty) CommentsText.Text = "Введите номер этажа!";
             if (float.TryParse(FloorNumberText.Text,out var floor) && float.TryParse(WeightText.Text, out var weight))
             {
                 result = floorAscent.Calculate(
@@ -98,6 +104,10 @@ namespace WpfApp2
                     (bool)LargeCheckBox.IsChecked,
                     floor,
                     weight);
+                if (weight <= 0 )
+                {
+                    CommentsText.Text = "Введите вес!";
+                }
             }
             return result;
         }
@@ -119,6 +129,16 @@ namespace WpfApp2
                 result = Math.Floor(assemblyCalculator.Calculate((bool)IsKitchenCheckBox.IsChecked, cost));
             }
             ResultText_Assembly.Text = result.ToString();
+        }
+
+        private void KmCountText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ClearResults();
+        }
+
+        private void WeightText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ClearResults();
         }
     }
 }
